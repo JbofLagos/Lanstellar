507616934c5a84eba8c05fb9e623b52928771ec5
victor <victorifeanyichukwu4@gmail.com>
2024-10-17 10:40:19 +0100

    changed logo on all navbars and the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 "use client";
 import React from 'react'
 import Image from 'next/image'
-import BgImage from './assets/images/start-bg.png'
+// import BgImage from './assets/images/start-bg.png'
+import lanstel from "./assets/images/lanstel-bg.png"
 import Navbar from './_components/navbar/navbar'
 import { Coinbase, Metamask, WalletConnect } from './assets/icons/icons'
 import DefaultButton from './_components/buttons/defaultButton';
@@ -10,56 +11,59 @@ import DefaultButton from './_components/buttons/defaultButton';
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col h-screen '>
-      <div className='z-10 fixed w-full h-[88px]'>
+    <div className="flex h-screen flex-col">
+      <div className="fixed z-10 h-[88px] w-full">
         <Navbar />
       </div>
 
-      <div className='flex xl:flex-row flex-col flex-grow justify-center items-center w-full h-screen text-white'>
-        <div className='xl:w-6/12 w-full bg-[#1C092A] h-screen flex  flex-col flex-grow items-center justify-center wallet-bg relative'>
-          <Image src={BgImage} alt='flower' className='w-full h-full object-cover' />
+      <div className="flex h-screen w-full flex-grow flex-col items-center justify-center text-white xl:flex-row">
+        <div className="wallet-bg relative flex h-screen w-full flex-grow flex-col items-center justify-center bg-[#1C092A] xl:w-6/12">
+          <Image
+            src={lanstel}
+            alt="flower"
+            className="h-full w-full object-cover"
+          />
 
-          <div className='w-3/12 ml-8 flex items-center justify-center absolute bottom-4'>
-                <DefaultButton
-                  addClass='w-6/12'
-                  className="default-btn"
-                  to="/role"
-                >
-                  Get Started
-                </DefaultButton>
+          <div className="absolute bottom-4 ml-8 flex w-3/12 items-center justify-center">
+            <DefaultButton addClass="w-6/12" className="default-btn" to="/role">
+              Get Started
+            </DefaultButton>
           </div>
         </div>
-        <div className='xl:w-6/12 w-full bg-[#230C33] h-screen flex flex-col flex-grow items-center justify-center gap-4 '>
-          <div className="flex flex-col justify-center items-center text-center">
-            <h3 className='text-white xl:text-[40px] text-[30px] font-bold font-montserrat leading-[48px]'>Connect wallet</h3>
-            <span className='xl:text-[24px] text-[16px] text-[white] font-normal xl:max-w-[600px] max-w-[400px] font-montserrat leading-[32px]'>
-              Choose a wallet you want to connect. There are several wallet providers.
+        <div className="flex h-screen w-full flex-grow flex-col items-center justify-center gap-4 bg-[#230C33] xl:w-6/12">
+          <div className="flex flex-col items-center justify-center text-center">
+            <h3 className="font-montserrat text-[30px] font-bold leading-[48px] text-white xl:text-[40px]">
+              Connect wallet
+            </h3>
+            <span className="max-w-[400px] font-montserrat text-[16px] font-normal leading-[32px] text-[white] xl:max-w-[600px] xl:text-[24px]">
+              Choose a wallet you want to connect. There are several wallet
+              providers.
             </span>
           </div>
-            <div className='flex flex-col gap-6 xl:w-6/12 w-10/12 mt-8'>
-                <div className='flex flex-row px-6 py-4 gap-4 items-center border border-[#fff] rounded-[20px]'>
-                    <span>
-                        <Metamask />
-                    </span>
-                    <span>Metamask</span>
-                </div>
-                {/* <div className='flex flex-row px-6 py-4 gap-4 items-center border border-[#fff] rounded-[20px]'>
+          <div className="mt-8 flex w-10/12 flex-col gap-6 xl:w-6/12">
+            <div className="flex flex-row items-center gap-4 rounded-[20px] border border-[#fff] px-6 py-4">
+              <span>
+                <Metamask />
+              </span>
+              <span>Metamask</span>
+            </div>
+            {/* <div className='flex flex-row px-6 py-4 gap-4 items-center border border-[#fff] rounded-[20px]'>
                     <span>
                        <WalletConnect />
                     </span>
                     <span>Wallet Connect</span>
                 </div> */}
-                <div className='flex flex-row px-6 py-4 gap-4 items-center border border-[#fff] rounded-[20px]'>
-                    <span>
-                        <Coinbase />
-                    </span>
-                    <span>Coinbase</span>
-                </div>
+            <div className="flex flex-row items-center gap-4 rounded-[20px] border border-[#fff] px-6 py-4">
+              <span>
+                <Coinbase />
+              </span>
+              <span>Coinbase</span>
             </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default HomePage
