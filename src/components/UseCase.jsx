968d84d4967bb923fd6b6bd8d5cf420644819106
oxbryte <silascyrax@gmail.com>
2025-03-21 22:26:36 +0100

    feat: enhance UseCase component layout and add new Stocks and Bonds section

--- src/components/UseCase.jsx
+++ src/components/UseCase.jsx
@@ -5,19 +5,23 @@ export default function UseCase() {
   const { isDarkMode } = useTheme();
   return (
     <div className="mx-auto w-full my-20 min-h-[60dvh] space-y-10">
-        <div className="w-full text-center">
-            <h1 className={`font-bold text-[24px] ${
+      <div className="w-full text-center">
+        <h1
+          className={`font-bold text-[24px] ${
             isDarkMode ? "text-white" : "text-[#24223E]"
-          }`}>Use Case</h1>
-        </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          }`}
+        >
+          Use Case
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto max-w-2xl gap-6">
         <div
           className={`space-y-3 items-left ${
             isDarkMode ? "text-white" : "text-[#24223E]"
           }`}
         >
           <div className="w-full h-fit rounded-lg overflow-hidden">
-            <img src="estate.jpg" alt="" className="object-cover" />
+            <img src="estate.jpg" alt="" className="object-cover h-[220px]" />
           </div>
           <p className="font-semibold text-2xl">Real Estate</p>
           <p className="font-light text-sm">
@@ -30,7 +34,7 @@ export default function UseCase() {
           }`}
         >
           <div className="w-full h-fit rounded-lg overflow-hidden">
-            <img src="art.jpg" alt="" className="object-cover" />
+            <img src="art.jpg" alt="" className="object-cover h-[220px]" />
           </div>
           <p className="font-semibold text-2xl">Art and Collectibles</p>
           <p className="font-light text-sm">
@@ -43,11 +47,28 @@ export default function UseCase() {
           }`}
         >
           <div className="w-full h-fit rounded-lg overflow-hidden">
-            <img src="commodity.jpg" alt="" className="object-cover" />
+            <img
+              src="commodity.jpg"
+              alt=""
+              className="object-cover h-[220px]"
+            />
           </div>
           <p className="font-semibold text-2xl">Commodities</p>
           <p className="font-light text-sm">
             Assess the value of oil, minerals, or farmland with geolocated data.
+          </p>
+        </div>
+        <div
+          className={`space-y-3 items-left ${
+            isDarkMode ? "text-white" : "text-[#24223E]"
+          }`}
+        >
+          <div className="w-full h-fit rounded-lg overflow-hidden">
+            <img src="stocks.jpg" alt="" className="object-cover h-[220px]" />
+          </div>
+          <p className="font-semibold text-2xl">Stocks and Bonds</p>
+          <p className="font-light text-sm">
+            Verify property prices, titles and zoning laws before investing.
           </p>
         </div>
       </div>
